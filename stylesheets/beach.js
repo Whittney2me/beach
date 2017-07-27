48674f6a187e3279aa11e910d77757d95f1d3f51
Whittney Twomey <wtwome13@gmail.com>
2017-07-27 14:47:20 -0700

    Lots of Changes, added bootstrap and edited the index page.

--- stylesheets/beach.js
+++ stylesheets/beach.js
@@ -4,8 +4,4 @@ $(document).ready(function() {
     $('.menu-section').toggleClass("on");
     $("nav ul").toggleClass('hidden');
   });
-    $(window).load(function() {
-    $(".loader").fadeOut(2800);
-    $(".box").fadeIn(4000);
-  });
 });
